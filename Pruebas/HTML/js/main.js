35e2b5413feb67f22f3f92f271ad416762f8f591
Jezumi <jmmb1870@gmail.com>
2024-02-07 15:01:20 +0100

    grid y demas

---
+++ Pruebas/HTML/js/main.js
@@ -0,0 +1,4 @@
+// Esto es un comentario en JS
+document.querySelector("#textoTerciario").textContent =
+  "Sos re putooooooooooooooooooooo";
+document.querySelector("#textoTerciario").textContent = "Sos re puto";
