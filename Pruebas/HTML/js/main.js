26b203686b2badb1d80ac6a762fc369f7957e703
JSalram <javisr997@gmail.com>
2024-02-22 17:31:49 +0100

    Correcciones Tarea 2 CSS

--- Pruebas/HTML/js/main.js
+++
@@ -1,4 +0,0 @@
-// Esto es un comentario en JS
-document.querySelector("#textoTerciario").textContent =
-  "Sos re putooooooooooooooooooooo";
-document.querySelector("#textoTerciario").textContent = "Sos re puto";
