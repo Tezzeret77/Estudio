87ea09442bb72be4b0a1dad65de5d5971e4da279
Jezumi <jmmb1870@gmail.com>
2024-04-17 14:09:34 +0200

    Color changer retocado

--- tareas/color-changer/main.js
+++ tareas/color-changer/main.js
@@ -2,6 +2,7 @@
     const square = document.getElementById("square");
     const colorButtons = document.querySelectorAll(".color-button");
     const colorText = document.getElementById ("color-text");
+    const customButton = document.querySelector(".custom-button");
   
     colorButtons.forEach(button => {
       button.addEventListener("click", function() {
@@ -10,3 +11,11 @@
         colorText.textContent = "Color seleccionado: " + color;
       });
     });
+
+
+    customButton.addEventListener("click", function() {
+      const customText = "Ya te dije que no hacia nada"; // Establecer el texto personalizado aquí
+      document.getElementById("color-text").textContent = customText;
+      alert("Eres tonto?");
+    });
+
